ecafab70402355a469be58d11bc80e8df6e52cf3
Christa <christanewing47@gmail.com>
2022-08-07 23:42:49 -0400

    update html and css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@ let apiKey = "e0d29c8a3a829feac5473c2221352f54";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0d29c8a3a829feac5473c2221352f54";
 
-// let city = prompt("Enter City");
-// city = city.toLowerCase();
+let name = prompt("What is your first name?");
 
 // if (weather[city] !== undefined) {
 //   let temperature = weather[city].temp;
