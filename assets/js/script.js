057044d8e67e404798b6d98d1fcd075d0fa5b74e
Christa <christanewing47@gmail.com>
2022-08-07 21:30:28 -0400

    populate files with starter code and link them together

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let apiKey = "e0d29c8a3a829feac5473c2221352f54";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e0d29c8a3a829feac5473c2221352f54";
+
+// let city = prompt("Enter City");
+// city = city.toLowerCase();
+
+// if (weather[city] !== undefined) {
+//   let temperature = weather[city].temp;
+//   let humidity = weather[city].humidity;
+//   let celciusTemperature = Math.round(temperature);
+//   let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+//   alert(
+//     `It is currently ${celciusTemperature}℃ (${fahrenheitTemperature}℉) in ${city} with a humidity of ${humidity}%`
+//   );
+// } else {
+//   alert(
+//     `Sorry we don't have the weather for this city, try going to htpps://www.google.com/?q=weather+${city}`
+//   );
+// }
